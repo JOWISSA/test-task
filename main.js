0130ee1ddbd71800f51e2644339eecb0a2626059
Kurachenko Vladislav <x0674016244@gmail.com>
2023-03-29 23:11:57 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -6,13 +6,22 @@ const table = document.querySelector('table');
 const deleteButton = document.getElementById('delete-button');
 const sortByNameBtn = document.querySelector('#sort-name-button');
 const sortByValueBtn = document.querySelector('#sort-value-button');
+const xmlBtn = document.querySelector('#xml-button');
+const saveBtn = document.querySelector('#save-button');
 
-addButton.addEventListener('click', () => {
+
+const addToInput = () => {
   const inputValues = nameValueInput.value.split('=').map(value => value.trim());
   const [name, value] = inputValues;
   
-  if (!name || !value) {
-    alert('input a valid format, looks like <neme>=<value>');
+  const isValid = (inputValues) => {
+    const [name, value] = inputValues.split('=').map(str => str.trim());
+    const regex = /^[a-zA-Z0-9-]+$/;
+    return regex.test(name) && regex.test(value);
+  }
+  
+  if (!name || !value || !isValid(name + '=' + value)) {
+    alert('entered must have only letters or numbers and look in the format <name>=<value>');
     return;
   }
   
@@ -27,10 +36,15 @@ addButton.addEventListener('click', () => {
   tableBody.appendChild(row);
 
   nameValueInput.value = '';
+}
+
+addButton.addEventListener('click', addToInput);
+
+document.addEventListener( 'keyup', event => {
+  if( event.code === 'Enter' ) {
+    addToInput()
+  };
 });
-
-// добавдение сортировки по имени
-
 
 sortByNameBtn.addEventListener('click', () => {
   const tbody = document.querySelector('tbody');
@@ -91,3 +105,68 @@ table.addEventListener('dblclick', (event) => {
 
   row.classList.remove('selected');
 });
+
+saveBtn.addEventListener('click', () => {
+  const rows = tableBody.querySelectorAll('tr');
+  const xmlDoc = document.implementation.createDocument(null, 'root', null);
+
+  const items = xmlDoc.createElement('items');
+  xmlDoc.documentElement.appendChild(items);
+
+  rows.forEach(row => {
+    const item = xmlDoc.createElement('item');
+    items.appendChild(item);
+
+    const name = xmlDoc.createElement('name');
+    name.textContent = row.cells[0].innerText;
+    item.appendChild(name);
+
+    const value = xmlDoc.createElement('value');
+    value.textContent = row.cells[1].innerText;
+    item.appendChild(value);
+  });
+
+  const serializer = new XMLSerializer();
+  const xmlString = serializer.serializeToString(xmlDoc);
+
+  const xmlBlob = new Blob([xmlString], { type: 'application/xml' });
+  const xmlUrl = URL.createObjectURL(xmlBlob);
+  const downloadLink = document.createElement('a');
+  downloadLink.href = xmlUrl;
+  downloadLink.download = 'data.xml';
+  document.body.appendChild(downloadLink);
+  downloadLink.click();
+  document.body.removeChild(downloadLink);
+  URL.revokeObjectURL(xmlUrl);
+});
+
+xmlBtn.addEventListener('click', () => {
+  const xmlDoc = document.implementation.createDocument(null, 'root', null);
+
+  const rootElement = xmlDoc.documentElement;
+
+  const tableRows = tableBody.querySelectorAll('tr');
+
+  tableRows.forEach((row) => {
+    const itemElement = xmlDoc.createElement('item');
+
+    const nameElement = xmlDoc.createElement('name');
+    const nameValue = row.cells[0].textContent;
+    nameElement.textContent = nameValue;
+
+    const valueElement = xmlDoc.createElement('value');
+    const valueText = row.cells[1].textContent;
+    valueElement.textContent = valueText;
+
+    itemElement.appendChild(nameElement);
+    itemElement.appendChild(valueElement);
+
+    rootElement.appendChild(itemElement);
+  });
+
+  const serializer = new XMLSerializer();
+  const xmlString = serializer.serializeToString(xmlDoc);
+
+  window.open(xmlString);
+});
+
